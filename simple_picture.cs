8a1ed5d3937b6eaec7ce5ce8c2fa10519802025b
Ferhad <f.3.d.1.a@mail.ru>
2023-07-29 15:28:09 +0400

    Triangle

---
+++ simple_picture.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Hello
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   .-.");
+            Console.WriteLine("  (o o)");
+            Console.WriteLine("  | O |");
+            Console.WriteLine("  |   |");
+            Console.WriteLine("  '~~~'");
+
+            Console.ReadLine();
+        }
+    }
+}
